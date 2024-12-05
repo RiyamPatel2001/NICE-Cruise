4b9520abc05e1e1cdf715e3daec1f908da04d32d
adgonzalez2018 <adgonzalez7@yahoo.com>
2024-12-04 19:25:54 -0500

    Update frontend folder

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "./SignIn";
+import SignIn from "./Signin";
 import HomePage from "./HomePage";
 import SuccessPage from "./Success";
 
